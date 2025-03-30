ab3e9cd94a22c1d08860787722bb8b6bf530a836
Morgan Trotter <morganhtrotter@gmail.com>
2025-03-29 17:09:40 -0700

    Matching base url to github repo name

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/vue-portfolio-2025/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
