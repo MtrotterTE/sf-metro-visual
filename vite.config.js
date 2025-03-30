739346133d3f8c5d0cdc5817f892c8f46ede07eb
Morgan Trotter <35540710+Morganhtrotter@users.noreply.github.com>
2025-03-29 17:48:10 -0700

    Change base url to match repository name

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/vue-portfolio-2025/',
+  base: '/vue-gh-pages-template/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
