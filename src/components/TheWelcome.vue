4926bfde53bd7460b3300f04db4378658fe0dcfc
Morgan Trotter <te.mtrotter@gmail.com>
2025-06-02 20:56:37 -0700

    Added D3 svg area

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,13 +1,40 @@
-<script setup>
-
-</script>
-
 <template>
   <div class="welcome">
     <h1 class="page-title">Tenco CityScale K Line Visualization</h1>
-  </div>
-  
+    <div ref="chart" class="svg-area"></div>
+  </div>  
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import * as d3 from 'd3';
+
+const chart = ref(null);
+
+onMounted(() => {
+  const width = 800;
+  const height = 2400;
+
+  const svg = d3.select(chart.value)
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+  svg.append('rect')
+    .attr('x', 380)
+    .attr('y', 0)
+    .attr('width', 10)
+    .attr('height', height)
+    .attr('fill', 'steelblue');
+
+  svg.append('rect')
+    .attr('x', 420)
+    .attr('y', 0)
+    .attr('width', 10)
+    .attr('height', height)
+    .attr('fill', 'steelblue');
+})
+</script>
 
 <style scoped>
 .page-title {
@@ -15,5 +42,13 @@
   font-size: 2rem;
   color: #C3BFBA;
   text-decoration: underline;
+}
+
+.svg-area {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: fit-content;
+  border: 1px solid #ccc;
 }
 </style>
